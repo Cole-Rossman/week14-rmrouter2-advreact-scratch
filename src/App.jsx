00cf702223e5e438d0e505a87469eb274c1186b1
Cole Rossman <colerossman99@gmail.com>
2022-05-09 11:21:24 -0700

    app.jsx and index.jsx complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Main from './views/Main/Main';
+import CharacterDetail from './views/CharacterDetail/CharacterDetail';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+  <>
+  <Header />
+  <Switch>
+    <Route path="/character/:id">
+      <CharacterDetail />
+    </Route>
+    <Route path="/">
+      <Main />
+    </Route>
+  </Switch>
+  </>
+  )
 }
