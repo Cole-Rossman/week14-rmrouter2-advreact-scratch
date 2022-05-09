6aa89585a4baca4b7760056fe966dacfae00d6a2
Cole Rossman <colerossman99@gmail.com>
2022-05-09 11:56:07 -0700

    useEffect added to main.js

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,7 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import mainStyle from './Main.css';
+import { useHistory, useLocation } from 'react-router-dom';
 
 
 export default function Main() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+  try {
+    const fetchData = async () => {
+        setLoading(true);
+        const statusParam = new URLSearchParams(location.search).get('status');
+        const url =
+        statusParam === 'all' || !statusParam
+        ? 'https://rickandmortyapi.com/api/character'
+        : `https://rickandmortyapi.com/api/character?status=${statusParam}`;
+        const res = await fetch(url);
+        const { results } = await res.json();
+        setCharacters(results);
+        setLoading(false);
+    }
+    fetchData();
+  } catch (e) {
+      setError(e.message);
+  }
+  }, []);
   return (
     <div>Main</div>
   )
